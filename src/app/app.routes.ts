89b1974e537d519c8fa4459d7ae46333663b716e
DevCaioH <caiocontatovagas@gmail.com>
2024-11-07 12:09:36 -0300

    Novas Edições

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { BodyComponent } from './frontend/body-exemple/body.component';
+import { PacientesBodyComponent } from './frontend/pacientes/pacientes-body/pacientes-body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BodyComponent },  // Rota padrão
+  { path: 'pacientes', component: PacientesBodyComponent },  // Rota para pacientes
+];
--- src/app/frontend/program/program.component.ts
+++ src/app/frontend/program/program.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
-import { BodyComponent } from "../body/body.component";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-program',
   standalone: true,
-  imports: [NavbarComponent, BodyComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './program.component.html',
   styleUrl: './program.component.css'
 })
