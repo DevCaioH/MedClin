d5dd1d87a21c53d400b96a9a750d3b4ef30472ba
DevCaioH <caiocontatovagas@gmail.com>
2024-11-12 13:12:30 -0300

    Criação da página de login e da responsívidade do logo da Winter's Technologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ProgramComponent } from "./frontend/program/program.component";
 
 import feather from 'feather-icons';
+import { LoginSystemComponent } from "./frontend/program/login-system/login-system.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProgramComponent, RouterOutlet],
+  imports: [ProgramComponent, RouterOutlet, LoginSystemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
